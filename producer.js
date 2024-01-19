70c05c875811a1d5971b117177e026682825f179
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-19 18:37:19 -0300

    producer

--- producer.js
+++ producer.js
@@ -1,27 +1,16 @@
 const express = require("express");
-const amqp = require("amqplib");
-
 const app = express();
-const PORT = 8000;
-const QUEUE_NAME = "exampleQueue";
+const queue = require("./queue");
 
 app.use(express.json());
 
-const startProducer = async () => {
-  const connection = await amqp.connect("amqp://localhost");
-  const channel = await connection.createChannel();
+const router = express.Router();
 
-  channel.assertQueue(QUEUE_NAME, { durable: true });
+router.post("/task", (req, res) => {
+  queue.sendToQueue("fila1", req.body);
+  res.json({ message: "Your request will be processed!" });
+});
 
-  app.post("/send-message", async (req, res) => {
-    const message = req.body.message;
-    channel.sendToQueue(QUEUE_NAME, Buffer.from(message), { persistent: true });
-    res.json({ status: "Message sent to the queue" });
-  });
+app.use("/", router);
 
-  app.listen(PORT, () => {
-    console.log(`Producer is running on http://localhost:${PORT}`);
-  });
-};
-
-startProducer();
+app.listen(3000);
